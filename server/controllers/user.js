3ffc67b746dfa3d5c6464252c09492ad1bf9a211
gio797 <g.muchaidze@yahoo.com>
2023-11-13 12:43:06 +0400

    add more crud

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -21,9 +21,67 @@ export const update = async (req, res, next) => {
     return next(createError(403, "You can only update your account!"));
   }
 };
-export const deleteUser = (req, res, next) => {};
-export const getUser = (req, res, next) => {};
-export const subscribe = (req, res, next) => {};
-export const unsubscribe = (req, res, next) => {};
-export const like = (req, res, next) => {};
-export const dislike = (req, res, next) => {};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User Deleted!");
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "You can only delete your account!"));
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(req.params.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+    res.status(200).json("Subscription successful!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const unsubscribe = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(req.params.id, {
+      $pull: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 },
+    });
+    res.status(200).json("Unsubscribed successfully!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const like = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const dislike = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
