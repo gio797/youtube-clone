cf260ea08abb13d56c216610b8798d685b0d017b
gio797 <g.muchaidze@yahoo.com>
2023-11-14 15:35:23 +0400

    style cards

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -7,11 +7,60 @@ const Container = styled.div`
   width: 360px;
   margin-bottom: 45px;
   cursor: pointer;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 202px;
   background-color: gray;
 `;
 
+const Details = styled.div`
+  display: flex;
+  margin-top: 16px;
+  gap: 12px;
+`;
+
+const ChannelImage = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 51%;
+  background-color: gray;
+`;
+
+const Texts = styled.div``;
+
+const Title = styled.h1`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.h2`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+  margin-block: 9px;
+`;
+
+const Info = styled.p`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
 function Card({}: Props) {
-  return <Container>Card</Container>;
+  return (
+    <Container>
+      <Image />
+      <Details>
+        <ChannelImage />
+        <Texts>
+          <Title>Test Video</Title>
+          <ChannelName>Gio's Channel</ChannelName>
+          <Info>Lorem ipsum dolor sit amet.</Info>
+        </Texts>
+      </Details>
+    </Container>
+  );
 }
 
 export default Card;
